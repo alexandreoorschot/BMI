4700ffb67be015319f7d0f3e972e2cde47b9e948
alexandreoorschot <alevan3818@gmail.com>
2017-07-05 20:20:33 -0300

    finished

--- app/src/main/java/br/com/gmit/myfirstapp/MainActivity.java
+++ app/src/main/java/br/com/gmit/myfirstapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -12,12 +14,16 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn_compute = (Button) findViewById(R.id.btn_compute);
+        final EditText edit_height = (EditText)findViewById(R.id.edit_height);
+        final EditText edit_weight = (EditText)findViewById(R.id.edit_weight);
+        final TextView txt_result = (TextView)findViewById(R.id.txt_result);
         btn_compute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 String height = edit_height.getText().toString();
-                String weight = edit_weight.getText().soString();
+                String weight = edit_weight.getText().toString();
                 if(height.length()>0 && weight.length()>0) {
+
                     float heightValue = Float.parseFloat(height);
                     float weightValue = Float.parseFloat(weight);
                     String result = calculateBMI(weightValue, heightValue);
@@ -35,6 +41,13 @@ public class MainActivity extends AppCompatActivity{
             result = "Your BMI: " + BMI + "(Severely underweight)";
         }else if (BMI < 18.5) {
             result = "your BMI: " + BMI + "(Underweight)";
+        }else if (BMI < 25 ) {
+            result = "Your BMI: " + BMI +"(Normal)";
+        }else if (BMI < 30) {
+            result = "Your BMI: " + BMI + "(Overweight)";
+        }else{
+            result = "your BMI: " + BMI + "(Obese)";
         }
+        return result;
     }
 }
