f475e068db1c6935dbac3f14f827b6e832143f80
alexandre <alevan3818@gmail.com>
2017-07-04 22:38:22 -0300

    segundo commit

--- app/src/main/java/br/com/gmit/myfirstapp/MainActivity.java
+++ app/src/main/java/br/com/gmit/myfirstapp/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.gmit.myfirstapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_compute = (Button) findViewById(R.id.btn_compute);
+        btn_compute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                String height = edit_height.getText().toString();
+                String weight = edit_weight.getText().soString();
+                if(height.length()>0 && weight.length()>0) {
+                    float heightValue = Float.parseFloat(height);
+                    float weightValue = Float.parseFloat(weight);
+                    String result = calculateBMI(weightValue, heightValue);
+                    txt_result.setText(result);
+                }
+            }
+        });
+    }
+    private String calculateBMI(float weigth, float height) {
+        height = (height / 100);
+        float BMI = weigth / (height * height);
+        String result;
+
+        if (BMI < 16) {
+            result = "Your BMI: " + BMI + "(Severely underweight)";
+        }else if (BMI < 18.5) {
+            result = "your BMI: " + BMI + "(Underweight)";
+        }
     }
 }
